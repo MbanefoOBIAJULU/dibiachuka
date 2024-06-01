0bad1f01295c51d86fb702a240a43dc87eef6dbf
mbanefoobiajulu <dibiaezueobiajulu@yahoo.com>
2024-06-01 15:47:06 +0100

    .

--- collabo2.js
+++ collabo2.js
@@ -1,1 +1,34 @@
-good
+Create a function that will output the first 100 prime numbers.*/
+
+function generatePrimes(count) {
+    let primes = [];
+    let num = 2; // Starting from 2, the first prime number
+
+    while (primes.length < count) {
+        if (isPrime(num)) {
+            primes.push(num);
+        }
+        num++;
+    }
+    return primes;
+}
+function isPrime(n) {
+    if (n <= 1) {
+        return false;
+    }
+    if (n <= 3) {
+        return true;
+    }
+    if (n % 2 === 0 || n % 3 === 0) {
+        return false;
+    }
+    let i = 5;
+    while (i * i <= n) {
+        if (n % i === 0 || n % (i + 2) === 0) {
+            return false;
+        }
+        i += 6;
+    }
+    return true;
+}
+console.log(generatePrimes(200)); // an array containing the first 100 prime numbers
